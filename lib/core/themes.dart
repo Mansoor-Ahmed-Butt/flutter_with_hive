877ef8a7cf59b3c47eae1e6887c47198e7727a48
Mansoor Ahmed <135522962+Mansoor-Ahmed-Butt@users.noreply.github.com>
2025-09-19 13:59:25 +0500

    added dependencies

---
+++ lib/core/themes.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color textColor = Color(0xFFFFFFFF);
+  static const Color textColorLabel = Color(0xFFF34E3A);
+  static const Color subtitleColor = Color(0xFF656565);
+  static const Color buttonColor = Color(0xFFF34E3A);
+  static const Color buttonTextColor = Color(0xFFF34E3A);
+  static const Color buttonBackgroundColor = Color(0xFF140702);
+  static const Color appBackgroundColor = Color(0xFF141516);
+  static const Color textFieldBackgroundColor = Color(0xFF090909);
+  static const Color cursorColor = Color(0xFF484848);
+  static const Color hintTextColor = Color(0xFF484848);
+  static const Color loginButtonColor = Color(0xFF008DC9);
+  static const Color selectionColor = Color(0xFF484848);
+  static const Color selectionHandleColor = Color(0xFFF34E3A);
+  static const Color homePageTextColor = Color(0xFFDAD6D6);
+  static const Color homePageDateColor = Color(0xFF545454);
+  static const Color dotColor = Color(0xFFDDDDDD);
+  static const Color currentStatusColor = Color(0xFF080808);
+  static const Color currentStatusTextColor = Color(0xFF727272);
+  static const Color lineChartLabelColor = Color(0xFF545454);
+  static const Color currentStatusLabelColor = Color(0xFF545454); 
+  static const Color activityColor = Color(0xFFDAD6D6); 
+  static const Color chooseProgramColor = Color(0xFF545454); 
+
+}
